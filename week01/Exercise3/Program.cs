b1025610ecb4fd642ab2ceac8b0a835f2ddd2b06
rogelioguerra24 <gue24018@byui.edu>
2025-03-08 18:52:12 -0500

    Third Excercise Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int newnumber = randomGenerator.Next(1, 10);
+
+        int guess = -1;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (newnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (newnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != newnumber);
     }
 }
