3592696e21813c116727f160e84b6c3a7e423533
rogelioguerra24 <gue24018@byui.edu>
2025-03-18 17:52:25 -0500

    New Program with Classes

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        
+        Fraction newFraction = new Fraction();
+        newFraction.GetFractionString();
+        newFraction.GetDecimalValue();
+        newFraction.SetTop(5);
+        newFraction.GetFractionString();
+        newFraction.GetDecimalValue();
+        newFraction.SetTop(3);
+        newFraction.SetBottom(4);
+        newFraction.GetFractionString();
+        newFraction.GetDecimalValue();
+        newFraction.SetTop(1);
+        newFraction.SetBottom(3);
+        newFraction.GetFractionString();
+        newFraction.GetDecimalValue();
     }
 }
